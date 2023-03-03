8a75b107e55d26b68a9ce1210c2d2be974559395
Arya Emini <arya@aryaemini.com>
2023-03-03 21:21:34 +0300

    initial proxy

---
+++ proxy/src/test/java/io/smartiq/aemini/proxy/ProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.smartiq.aemini.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
